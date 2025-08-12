21b3b0240553b420b31886df0eeadfa53699d2ff
vishnu teli <vishnusindhal5040@gmail.com>
2025-08-12 09:06:13 +0530

    updating the footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,16 +10,16 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by DeveloperFolio Team")}
+          {emoji("")}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          Theme by{" "}
+          
           <a
-            href="https://github.com/saadpasta/developerFolio"
+            href=""
             target="_blank"
             rel="noreferrer"
           >
-            developerFolio
+            
           </a>
         </p>
       </div>
